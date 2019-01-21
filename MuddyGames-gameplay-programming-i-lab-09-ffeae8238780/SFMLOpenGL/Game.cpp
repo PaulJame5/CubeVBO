10b78e9c64af2889bb98a5e540870ceac670ebc8
PaulJame5 <43470494+PaulJame5@users.noreply.github.com>
2019-01-21 18:20:53 +0000

    Add files via upload
    100% done

--- MuddyGames-gameplay-programming-i-lab-09-ffeae8238780/SFMLOpenGL/Game.cpp
+++ MuddyGames-gameplay-programming-i-lab-09-ffeae8238780/SFMLOpenGL/Game.cpp
@@ -1,27 +1,166 @@
 #include <Game.h>
 
-static bool flip;
+bool flip = false;
+int current = 1;
 
-Game::Game() : window(sf::VideoMode(800, 600), "OpenGL Cube VBO")
+Game::Game() : window(VideoMode(800, 600), "OpenGL Cube")
 {
+
 }
 
 Game::~Game() {}
+
+// We have 36 verts with 3 points
+float temporaryVerts[36 * 3];
+
+// Vertices for one Triangle
+float vertices[] = { 
+	-1.0f,-1.0f,-3, // t1
+	-1.0f,-1.0f, -5.0f,
+	-1.0f, 1.0f, -5.0f, // t1
+	1.0f, 1.0f,-3, // t2
+	-1.0f,-1.0f,-3,
+	-1.0f, 1.0f,-3, // t2
+	1.0f,-1.0f, -5.0f,//t3s
+	-1.0f,-1.0f,-3,
+	1.0f,-1.0f,-3,//t3e
+	1.0f, 1.0f,-3,//t4s
+	1.0f,-1.0f,-3,
+	-1.0f,-1.0f,-3,//t4
+	-1.0f,-1.0f,-3,//t5
+	-1.0f, 1.0f, -5.0f,
+	-1.0f, 1.0f,-3,//t5
+	1.0f,-1.0f, -5.0f,//t6
+	-1.0f,-1.0f, -5.0f,
+	-1.0f,-1.0f,-3,//t6
+	-1.0f, 1.0f, -5.0f,//t7
+	-1.0f,-1.0f, -5.0f,
+	1.0f,-1.0f, -5.0f,//t7
+	1.0f, 1.0f, -5.0f,//t8
+	1.0f,-1.0f,-3,
+	1.0f, 1.0f,-3,//t8
+	1.0f,-1.0f,-3,//t9
+	1.0f, 1.0f, -5.0f,
+	1.0f,-1.0f, -5.0f,//t9
+	1.0f, 1.0f, -5.0f,//t10
+	1.0f, 1.0f,-3,
+	-1.0f, 1.0f,-3,//t10
+	1.0f, 1.0f, -5.0f,//t11
+	-1.0f, 1.0f,-3,
+	-1.0f, 1.0f, -5.0f,//t11
+	1.0f, 1.0f, -5.0f,//t12
+	-1.0f, 1.0f, -5.0f,
+	1.0f,-1.0f, -5.0f//t12
+};
+
+	// copy of vertices for restoring vertices
+	float verticesCopy[] = {
+	-1.0f,-1.0f,-3, // t1
+	-1.0f,-1.0f, -5.0f,
+	-1.0f, 1.0f, -5.0f, // t1
+	1.0f, 1.0f,-3, // t2
+	-1.0f,-1.0f,-3,
+	-1.0f, 1.0f,-3, // t2
+	1.0f,-1.0f, -5.0f,//t3s
+	-1.0f,-1.0f,-3,
+	1.0f,-1.0f,-3,//t3e
+	1.0f, 1.0f,-3,//t4s
+	1.0f,-1.0f,-3,
+	-1.0f,-1.0f,-3,//t4
+	-1.0f,-1.0f,-3,//t5
+	-1.0f, 1.0f, -5.0f,
+	-1.0f, 1.0f,-3,//t5
+	1.0f,-1.0f, -5.0f,//t6
+	-1.0f,-1.0f, -5.0f,
+	-1.0f,-1.0f,-3,//t6
+	-1.0f, 1.0f, -5.0f,//t7
+	-1.0f,-1.0f, -5.0f,
+	1.0f,-1.0f, -5.0f,//t7
+	1.0f, 1.0f, -5.0f,//t8
+	1.0f,-1.0f,-3,
+	1.0f, 1.0f,-3,//t8
+	1.0f,-1.0f,-3,//t9
+	1.0f, 1.0f, -5.0f,
+	1.0f,-1.0f, -5.0f,//t9
+	1.0f, 1.0f, -5.0f,//t10
+	1.0f, 1.0f,-3,
+	-1.0f, 1.0f,-3,//t10
+	1.0f, 1.0f, -5.0f,//t11
+	-1.0f, 1.0f,-3,
+	-1.0f, 1.0f, -5.0f,//t11
+	1.0f, 1.0f, -5.0f,//t12
+	-1.0f, 1.0f, -5.0f,
+	1.0f,-1.0f, -5.0f//t12
+};
+
+
+// Colors for those vertices
+float colors[] = {
+	1.0f, 0.0f, 0.0f,
+	1.0f, 0.0f, 0.0f,
+	1.0f, 0.0f, 0.0f,
+
+	0.0f, 1.0f, 0.0f,
+	0.0f, 1.0f, 0.0f,
+	0.0f, 1.0f, 0.0f,
+
+	0.0f, 0.0f, 1.0f,
+	0.0f, 0.0f, 1.0f,
+	0.0f, 0.0f, 1.0f,
+
+	1.0f, 1.0f, 0.0f,
+	1.0f, 1.0f, 0.0f,
+	1.0f, 1.0f, 0.0f,
+
+	0.0f, 1.0f, 1.0f,
+	0.0f, 1.0f, 1.0f,
+	0.0f, 1.0f, 1.0f,
+
+	1.0f, 1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f,
+
+	1.0f, 0.0f, 1.0f,
+	1.0f, 0.0f, 1.0f,
+	1.0f, 0.0f, 1.0f,
+
+	0.0f, 0.5f, 0.0f,
+	0.0f, 0.5f, 0.0f,
+	0.0f, 0.5f, 0.0f,
+
+	0.0f, 0.0f, 0.5f,
+	0.0f, 0.0f, 0.5f,
+	0.0f, 0.0f, 0.5f,
+
+	1.0f, 0.5f, 0.0f,
+	1.0f, 0.5f, 0.0f,
+	1.0f, 0.5f, 0.0f,
+
+	0.5f, 1.0f, 0.0f,
+	0.5f, 1.0f, 0.0f,
+	0.5f, 1.0f, 0.0f,
+
+	0.5f, 0.5f, 1.0f,
+	0.5f, 0.5f, 1.0f,
+	0.5f, 0.5f, 1.0f,
+};
+
+// Index to be drawn
+unsigned int vertex_index[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35 };
 
 void Game::run()
 {
 
 	initialize();
 
-	sf::Event event;
+	Event event;
 
 	while (isRunning) {
 
-		//std::cout << "Game running..." << std::endl;
 
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed)
+			if (event.type == Event::Closed)
 			{
 				isRunning = false;
 			}
@@ -32,245 +171,264 @@ void Game::run()
 
 }
 
-typedef struct
-{
-	float coordinate[3];
-	float color[3];
-} Vertex;
-
-Vertex vertex[6];
-GLubyte triangles[6];
-
-/* Variable to hold the VBO identifier */
-GLuint vbo[1];
-GLuint index;
-
 void Game::initialize()
 {
 	isRunning = true;
-
-	glewInit();
-
-	/* Vertices counter-clockwise winding */
-
-	vertex[0].coordinate[0] = -0.5f;
-	vertex[0].coordinate[1] = -0.5f;
-	vertex[0].coordinate[2] = 0.0f;
-
-	vertex[1].coordinate[0] = -0.5f;
-	vertex[1].coordinate[1] = 0.5f;
-	vertex[1].coordinate[2] = 0.0f;
-
-	vertex[2].coordinate[0] = 0.5f;
-	vertex[2].coordinate[1] = 0.5f;
-	vertex[2].coordinate[2] = 0.0f;
-
-	vertex[3].coordinate[0] = 0.5f; 
-	vertex[3].coordinate[1] = 0.5f;  
-	vertex[3].coordinate[2] = 0.0f;
-
-	vertex[4].coordinate[0] = 0.5f; 
-	vertex[4].coordinate[1] = -0.5f;  
-	vertex[4].coordinate[2] = 0.0f;
-
-	vertex[5].coordinate[0] = -0.5f; 
-	vertex[5].coordinate[1] = -0.5f;  
-	vertex[5].coordinate[2] = 0.0f;
-
-	vertex[0].color[0] = 0.1f;
-	vertex[0].color[1] = 1.0f;
-	vertex[0].color[2] = 0.0f;
-
-	vertex[1].color[0] = 0.2f;
-	vertex[1].color[1] = 1.0f;
-	vertex[1].color[2] = 0.0f;
-
-	vertex[2].color[0] = 0.3f;
-	vertex[2].color[1] = 1.0f;
-	vertex[2].color[2] = 0.0f;
-
-	vertex[3].color[0] = 0.4f;
-	vertex[3].color[1] = 1.0f;
-	vertex[3].color[2] = 0.0f;
-
-	vertex[4].color[0] = 0.5f;
-	vertex[4].color[1] = 1.0f;
-	vertex[4].color[2] = 0.0f;
-
-	vertex[5].color[0] = 0.6f;
-	vertex[5].color[1] = 1.0f;
-	vertex[5].color[2] = 0.0f;
-
-
-	triangles[0] = 0;   triangles[1] = 1;   triangles[2] = 2;
-	triangles[3] = 3;   triangles[4] = 4;   triangles[5] = 5;
-
-	/* Create a new VBO using VBO id */
-	glGenBuffers(1, vbo);
-
-	/* Bind the VBO */
-	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-
-	/* Upload vertex data to GPU */
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * 6, vertex, GL_STATIC_DRAW);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	glGenBuffers(1, &index);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLubyte) * 6, triangles, GL_STATIC_DRAW);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	//glTranslatef(0, 0, 10);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0, window.getSize().x / window.getSize().y, 1.0, 1000.0);
+	glMatrixMode(GL_MODELVIEW);
 }
 
 void Game::update()
 {
 	elapsed = clock.getElapsedTime();
 
-	if (elapsed.asSeconds() >= 1.0f)
-	{
-		clock.restart();
-
-		if (!flip)
-		{
-			flip = true;
-		}
-		else
-			flip = false;
-	}
-
-	if (flip)
-	{
-		rotationAngle += 0.005f;
-
-		if (rotationAngle > 360.0f)
-		{
-			rotationAngle -= 360.0f;
-		}
-	}
-
-	//translate
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-	{
-		// vertex index
-		for (int i = 0; i < 6; i++)
-		{
-			MyVector3 tempVec = MyVector3{ vertex[i].coordinate[0], vertex[i].coordinate[1], vertex[i].coordinate[2] };
-			
-			MyVector3 displace = MyVector3{ 0, 0.0002f, 0 };
-
-			MyVector3 newVec = (MyMatrix3::translation(displace) * tempVec);
-
-			std::cout << "tempVec before: " << tempVec.toString() << std::endl;
-
-			//vertex[i] = tempVec;
-
-			vertex[i].coordinate[0] = newVec.x;
-			vertex[i].coordinate[1] = newVec.y;
-			vertex[i].coordinate[2] = newVec.z;
-
-
-			
-			std::cout << "placing" << i << std::endl;
-			
-			
-		}
-
-		//Change vertex data
-		//vertex[0].coordinate[0] += -0.0001f;
-		//vertex[0].coordinate[1] += -0.0001f;
-		//vertex[0].coordinate[2] += -0.0001f;
-	}
-
-
-	//scale
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-	{
-		// vertex index
-		for (int i = 0; i < 6; i++)
-		{
-			MyVector3 tempVec = MyVector3{ vertex[i].coordinate[0], vertex[i].coordinate[1], vertex[i].coordinate[2] };
-
-			MyVector3 displace = MyVector3{ 0, 0.0002f, 0 };
-
-			MyVector3 newVec = (MyMatrix3::scale(1.001f) * tempVec);
-
-			std::cout << "tempVec before: " << tempVec.toString() << std::endl;
-
-			//vertex[i] = tempVec;
-
-			vertex[i].coordinate[0] = newVec.x;
-			vertex[i].coordinate[1] = newVec.y;
-			vertex[i].coordinate[2] = newVec.z;
-
-
-
-
-			std::cout << "placing" << i << std::endl;
-
-
-		}
-	}
-
-	//scale
+	// Reset
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::R))
 	{
-		// vertex index
-		for (int i = 0; i < 6; i++)
+		for (int i = 0; i < 108; i++)
 		{
-			MyVector3 tempVec = MyVector3{ vertex[i].coordinate[0], vertex[i].coordinate[1], vertex[i].coordinate[2] };
-
-			MyVector3 displace = MyVector3{ 0, 0.0002f, 0 };
-
-			MyVector3 newVec = (MyMatrix3::rotationZ(1) * tempVec);
-
-			std::cout << "tempVec before: " << tempVec.toString() << std::endl;
-
-			//vertex[i] = tempVec;
-
-			vertex[i].coordinate[0] = newVec.x;
-			vertex[i].coordinate[1] = newVec.y;
-			vertex[i].coordinate[2] = newVec.z;
-
-
-
-
-			std::cout << "placing" << i << std::endl;
-
-
+			vertices[i] = verticesCopy[i];
 		}
 	}
 
+	// Rotation
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::X))
+	{
+		
 
-	//std::cout << "Update up" << std::endl;
+
+			// loop through 36 times
+			for (int i = 0; i < 36; i++)
+			{
+				// offsets for choosing index
+				int x = i * 3;
+				int y = i * 3 + 1;
+				int z = i * 3 + 2;
+				// temporary vector for performing calculations
+				MyVector3 tVect = MyVector3{ vertices[x],vertices[y],vertices[z] };
+
+				
+					// rotate along the x axis by 2 degrees
+				tVect = MyMatrix3::rotationX(rotationAngle) * tVect;
+				
+				vertices[x] = tVect.x;
+				vertices[y] = tVect.y;
+				vertices[z] = tVect.z;
+			}
+
+
+
+				
+
+	} // eND x
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Y))
+	{
+
+
+
+		// loop through 36 times
+		for (int i = 0; i < 36; i++)
+		{
+			// offsets for choosing index
+			int x = i * 3;
+			int y = i * 3 + 1;
+			int z = i * 3 + 2;
+			// temporary vector for performing calculations
+			MyVector3 tVect = MyVector3{ vertices[x],vertices[y],vertices[z] };
+
+
+			// rotate along the x axis by 2 degrees
+			tVect = MyMatrix3::rotationY(rotationAngle) * tVect;
+
+			vertices[x] = tVect.x;
+			vertices[y] = tVect.y;
+			vertices[z] = tVect.z;
+			//std::cout << i << std::endl;
+		}
+
+	}// end Y
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z))
+	{
+
+
+
+		// loop through 36 times
+		for (int i = 0; i < 36; i++)
+		{
+			// offsets for choosing index
+			int x = i * 3;
+			int y = i * 3 + 1;
+			int z = i * 3 + 2;
+			// temporary vector for performing calculations
+			MyVector3 tVect = MyVector3{ vertices[x],vertices[y],vertices[z] };
+
+			// rotate along the z axis by 0.2 degrees
+			tVect = MyMatrix3::rotationZ(rotationAngle) * tVect;
+
+			vertices[x] = tVect.x;
+			vertices[y] = tVect.y;
+			vertices[z] = tVect.z;
+			//std::cout << i << std::endl;
+		}
+
+
+
+
+
+	}//end Z
+		//END ROTATION
+
+
+	//scale
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+	{
+
+			// loop through 36 times
+			for (int i = 0; i < 36; i++)
+			{
+				// offsets for choosing index
+				int x = i * 3;
+				int y = i * 3 + 1;
+				int z = i * 3 + 2;
+				// temporary vector for performing calculations
+				MyVector3 tVect = MyVector3{ vertices[x],vertices[y],vertices[z] };
+
+
+				tVect = MyMatrix3::scale(.99) * tVect;
+				
+
+				vertices[x] = tVect.x;
+				vertices[y] = tVect.y;
+				vertices[z] = tVect.z;
+			}
+
+
+
+
+	} // end s press for scale
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+	{
+
+		// loop through 36 times
+		for (int i = 0; i < 36; i++)
+		{
+			// offsets for choosing index
+			int x = i * 3;
+			int y = i * 3 + 1;
+			int z = i * 3 + 2;
+			// temporary vector for performing calculations
+			MyVector3 tVect = MyVector3{ vertices[x],vertices[y],vertices[z] };
+
+
+			tVect = MyMatrix3::scale(1.01) * tVect;
+			
+
+			vertices[x] = tVect.x;
+			vertices[y] = tVect.y;
+			vertices[z] = tVect.z;
+		}
+
+		
+	} // END SCALE
+
+
+
+	// Move cube
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || 
+		sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+	{
+		//if (!pressedTransformation)
+		{
+
+			pressedTransformation = true;
+
+			// loop through 36 times
+			for (int i = 0; i < 36; i++)
+			{
+				// offsets for choosing index
+				int x = i * 3;
+				int y = i * 3 + 1;
+				int z = i * 3 + 2;
+				// temporary vector for performing calculations
+				MyVector3 tVect = MyVector3{ vertices[x],vertices[y],vertices[z] };
+				
+				// vector declared for moving
+				MyVector3 displacementAmount;
+
+				// temp vector for moving along y axis
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+				{
+
+					displacementAmount = MyVector3{ 0,-0.002,0 };
+				}
+
+				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+				{
+
+					displacementAmount = MyVector3{ 0,0.002,0 };
+
+				}
+
+				// set vector for moving along x axis
+				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+				{
+
+					displacementAmount = MyVector3{ 0.002,0,0 };
+				}
+
+				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+				{
+
+					displacementAmount = MyVector3{ -0.002,0,0 };
+
+				}
+
+
+
+				tVect = MyMatrix3::translation(displacementAmount) * tVect;
+
+				vertices[x] = tVect.x;
+				vertices[y] = tVect.y;
+				vertices[z] = tVect.z;
+			}
+
+
+
+
+		} // end !pressed
+
+
+	}
+	else
+	{
+		pressedTransformation = false;
+	}
+
+
+
 }
 
 void Game::render()
 {
-	//std::cout << "Drawing" << std::endl;
-
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-
-	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index);
-
-	/*	As the data positions will be updated by the this program on the
-		CPU bind the updated data to the GPU for drawing	*/
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * 6, vertex, GL_STATIC_DRAW);
-
 	glEnableClientState(GL_VERTEX_ARRAY);
 	glEnableClientState(GL_COLOR_ARRAY);
 
 
-	glColorPointer(3, GL_FLOAT, sizeof(Vertex), (char*)NULL + 12);
+	glVertexPointer(3, GL_FLOAT, 0, &vertices);
+	glColorPointer(3, GL_FLOAT, 0, &colors);
 
-	/*	Draw Triangle from VBO	(set where to start from as VBO can contain 
-		model compoents that are and are not to be drawn )	*/
-	glVertexPointer(3, GL_FLOAT, sizeof(Vertex), (char*)NULL + 0);
-	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, (char*)NULL + 0);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
 
-	glDisableClientState(GL_VERTEX_ARRAY);
+	//glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, &vertex_index);
+
 	glDisableClientState(GL_COLOR_ARRAY);
+	glDisableClientState(GL_VERTEX_ARRAY);
 
 	window.display();
 
@@ -278,8 +436,6 @@ void Game::render()
 
 void Game::unload()
 {
-	//std::cout << "Cleaning up" << std::endl;
-
-	glDeleteBuffers(1, vbo);
+	cout << "Cleaning up" << endl;
 }
 
--- MuddyGames-gameplay-programming-i-lab-09-ffeae8238780/SFMLOpenGL/Game.h
+++ MuddyGames-gameplay-programming-i-lab-09-ffeae8238780/SFMLOpenGL/Game.h
@@ -1,28 +1,39 @@
 #include <iostream>
-#include <GL/glew.h>
-#include <GL/wglew.h>
 #include <SFML/Window.hpp>
 #include <SFML/OpenGL.hpp>
+#include <gl/GL.h>
+#include <gl/GLU.h>
 
-#include <Vector3.h>
-#include <MyMatrix3.h>
+#include "Vector3.h"
+#include "MyMatrix3.h"
 
+using namespace std;
+using namespace sf;
+/// xcopy $(SFML_SDK\bin $(TargetDir) /C /Y ///
 class Game
 {
 public:
 	Game();
 	~Game();
 	void run();
+
+	bool pressedRotation = false;
+	bool pressedScale = false;
+	bool pressedTransformation = false;
+
+	MyVector3 t_displacement { 1.0f, 1.0f, 0.0f };
 private:
-	sf::Window window;
+	Window window;
 	bool isRunning = false;
 	void initialize();
 	void update();
 	void render();
 	void unload();
 
-	sf::Clock clock;
-	sf::Time elapsed;
+	Clock clock;
+	Time elapsed;
 
-	float rotationAngle = 0.0f;
+	float rotationAngle = 0.0001f;
+
+	float z = 10;
 };
